9d0545bb9e9e3b0c36d360d3ee0e8f0d1341f61b
xuguoyi <339583984@qq.com>
2022-06-05 13:16:26 +0800

    feat: init commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const { defaultTheme } = require('@vuepress/theme-default')
+module.exports = {
+  lang: 'zh-CN',
+  title: '你好， 大兄弟 ！',
+  description: '这是我的前端学习记录!',
+  base: '/',
+  open: true,
+  // head: [
+  //     ['link', {
+  //         rel: 'icon',
+  //         href: `/logo.png`
+  //     }]
+  // ],
+  // 如果你的对象只有那些 “常青树” 浏览器，你可以将其设置成 true 。这将会禁用一些转译过程和 Polyfills ，带来更快的构建速度和更小的文件体积。
+  evergreen: true,
+  alias: {
+    '@assets': path.resolve(__dirname, './assets'),
+  },
+  theme: defaultTheme({
+    logo: "/logo.png",
+    logoDark: "/logo-dark.png",
+    editLink: true,
+    editLinkText: "编辑此页",
+    repo: '9kg/blog',
+    repoLabel: "访问Github",
+    docsBranch: 'master',
+    docsDir: 'docs',
+    notFound: ['当前路径不存在~'],
+    lastUpdatedText: "最近更新",
+    contributors: false,
+    backToHome: '返回首页',
+    openInNewWindow: '新窗口打开',
+    toggleDarkMode: '昼夜交替',
+    toggleSidebar: '切换侧边栏',
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+    sidebar: {
+      '/guide/': [
+        {
+          text: '网络',
+          collapsible: true,
+          children: [
+            '/guide/a.md',
+            '/guide/b.md',
+          ],
+        },
+      ]
+    }
+  }),
+}
---
+++ docs/README.md
@@ -0,0 +1,28 @@
+---
+home: true
+# heroImage: /logo.png
+# heroImageDark: /logo-dark.png
+heroText: 好好学习
+tagline: 为中华民族之崛起而学习!
+actions:
+  - text: 开始学习
+    link: /guide/
+    type: primary
+  # - text: 读过的书
+  #   link: /books/
+  #   type: secondary
+  # - text: 高频面试题
+  #   link: /interview/
+  #   type: secondary
+features:
+- title: 数据结构
+  details: 集合, 链表, 队列, 栈, 堆, 树, 图
+- title: 算法
+  details: 排序, 查找, 模式匹配, 贪心, 动态规划, 回溯, 分治
+- title: 浏览器
+  details: 缓存机制, 请求流程, 渲染流程
+- title: VUE
+  details: 生命周期, diff算法, 实现原理
+footer:  9kg | Copyright © 2022-present(版权所有，转载请注来源)
+editLink: true
+---
